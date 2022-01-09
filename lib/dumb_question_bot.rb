a2cb21ee690694376f4ef6c067e0dcf047d39329
Jeetaditya Chatterjee <jeetelongname@gmail.com>
2022-01-09 23:27:50 +0000

    feat: finish of backend (for the most part)

--- lib/dumb_question_bot.rb
+++ lib/dumb_question_bot.rb
@@ -1,6 +1,30 @@
-require "dumb_question_bot/version"
+# frozen_string_literal: true
 
-module DumbQuestionBot
-  class Error < StandardError; end
-  # Your code goes here...
+require 'json'
+require 'dumb_question_bot/version'
+require 'sinatra'
+
+defaults = { title: 'some title', name: 'some instance name', description: 'some cool description' }
+
+data = JSON.parse(File.read('./conf.json')).map do |key, elem|
+  [key.to_sym, elem]
+end.to_h
+
+defaults.each do |key, elem|
+  data[key] = elem if data[key].nil?
+end
+
+set :views, "#{settings.root}/../views"
+
+get '/' do
+  erb :index, locals: {
+    title: data[:title],
+    name: data[:name],
+    description: data[:description]
+  }
+end
+
+post '/after' do
+  question = params[:question]
+  erb :after, locals: { question: question }
 end
